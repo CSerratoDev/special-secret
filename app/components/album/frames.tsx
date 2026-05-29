ab8d403dc46020732ad89e94ae20ef4f2a7436cd
cserratodev <cserrato.dev@gmail.com>
2026-05-29 17:09:51 -0600

    Más fotos juntos :3

--- app/components/album/frames.tsx
+++ app/components/album/frames.tsx
@@ -2,18 +2,24 @@
 
 import { useState } from "react";
 
-const PHOTO_PATHS = [
-    '/photos/primer_dia.jpg',
-    '/photos/bonita.jpg',
-    '/photos/preciosa.jpg',
-    '/photos/mua.jpg',
-    '/photos/nosotros.png',
-    '/photos/me_gustas.png',
-    '/photos/inicio.png',
-    '/photos/flor.png',
-    '/photos/cartita.png',
-    '/photos/mensajitos.jpg',
-    '/photos/ella.jpg'
+type Photo = {
+    src: string;
+    title: string;
+}
+
+const PHOTO_PATHS: Photo[] = [
+    { src: '/photos/primer_dia.jpg', title: 'Primer fotito 🥰' },
+    { src: '/photos/bonita.jpg', title: 'Bonitaaa 😍' },
+    { src: '/photos/preciosa.jpg', title: 'Preciosa 😳' },
+    { src: '/photos/mua.jpg', title: 'Mua 🥹' },
+    { src: '/photos/nosotros.png', title: 'Nosotros 🤓' },
+    { src: '/photos/me_gustas.png', title: 'Me gustas 🫣' },
+    { src: '/photos/inicio.png', title: 'Inicio 💌' },
+    { src: '/photos/flor.png', title: 'Esa flor 🌼' },
+    { src: '/photos/cartita.png', title: 'Cartita ❤️' },
+    { src: '/photos/mensajitos.jpg', title: 'Mensajitos 👀' },
+    { src: '/photos/ella.jpg', title: 'Tu Hermoso Rostro 🌷' },
+    { src: '/photos/me_flechaste.png', title: 'Me flechaste 💘' }
 ]
 
 const ITEMS_PER_PAGE = 6;
@@ -28,7 +34,7 @@ export default function Frames() {
     return (
         <div className="flex flex-col items-center max-w-2xl w-full p-4 gap-6">
             <div className="grid grid-cols-2 md:grid-cols-3 gap-6 max-w-2xl w-full p-4 animate-fade-in">
-                {currentPhotos.map((src, index) => {
+                {currentPhotos.map((photo, index) => {
                     const globalIndex = startIndex + index
                     return (
                         <div
@@ -37,14 +43,17 @@ export default function Frames() {
                         >
                             <div className='w-full aspect-square bg-gray-100 overflow-hidden relative rounded-sm border-black/5'>
                                 <img
-                                    src={src}
-                                    alt={`Recuerdo ${globalIndex + 1}`}
-                                    className='w-aut h-auto md:w-[200] md:h-[200] object-cover filter brightness-95 contrast-105'
+                                    src={photo.src}
+                                    alt={photo.title}
+                                    className='w-auto h-auto md:w-[200] md:h-[200] object-cover filter brightness-95 contrast-105'
                                     loading="lazy"
                                 />
                             </div>
 
-                            <div>
+                            <div className="w-full flex justify-between mt-2">
+                                <h3 className="font-light text-sm font-medium text-gray-600 tracking-wide">
+                                    {photo.title}
+                                </h3>
                                 <span className='font-serif text-xs text-gray-400 tracking-widest uppercase'>
                                     {globalIndex + 1} / {PHOTO_PATHS.length}
                                 </span>
